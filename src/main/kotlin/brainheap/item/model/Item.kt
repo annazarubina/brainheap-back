6183e5da1d8cb387ae679018e7bd0c246ca837a5
ekisali <alina.kisialiova@ericsson.com>
2019-05-02 17:09:56 +0100

    https://github.com/annazarubina/brainheap-back/issues/9 New fields have been added in Item data model.

--- src/main/kotlin/brainheap/item/model/Item.kt
+++ src/main/kotlin/brainheap/item/model/Item.kt
@@ -2,10 +2,17 @@ package brainheap.item.model
 
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
+import java.util.*
 
 data class Item(
         @field:NotEmpty
+        val id: String,
+        @field:NotEmpty
         val title: String,
         @field:NotNull
-        val description: String
-)
+        val description: String,
+        @field:NotNull
+        val created: Date,
+        @field:NotNull
+        val modified: Date
+        )
