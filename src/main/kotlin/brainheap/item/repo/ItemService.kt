8cba8505c52b888bb539ee1d2ebe3f864bc319cb
Anna Zarubina <anna.zarubina@ericsson.com>
2019-05-05 11:52:43 +0100

    #16 - LIKE was replesed by = in userId requests, fomating for Slava

--- src/main/kotlin/brainheap/item/repo/ItemService.kt
+++ src/main/kotlin/brainheap/item/repo/ItemService.kt
@@ -3,28 +3,29 @@ package brainheap.item.repo
 import brainheap.item.model.Item
 import org.hibernate.Session
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import javax.persistence.EntityManager
 
 @Service
 class ItemService(private val entityManager: EntityManager) {
-    private val templateUserIdAndQuery: String = "SELECT i FROM Item i WHERE i.userId LIKE '%s' AND (%s)"
+    private val templateUserIdAndQuery: String = "SELECT i FROM Item i WHERE i.userId = '%s' AND (%s)"
     private val templateQuery: String = "SELECT i FROM Item i WHERE (%s)"
-    private val templateUserId: String = "SELECT i FROM Item i WHERE i.userId LIKE '%s'"
+    private val templateUserId: String = "SELECT i FROM Item i WHERE i.userId = '%s'"
     private val templateAll: String = "SELECT i FROM Item i"
 
     fun filter(userId: String?, query: String?): List<Item>? =
-            runQuery(query?.let{userId
-                    ?.let { templateUserIdAndQuery.format(userId, query) } ?: templateQuery.format(query)}
-                    ?: userId?.let { templateUserId.format(userId) } ?: templateAll
+            runFilterQuery(query
+                    ?.let {
+                        userId
+                                ?.let { templateUserIdAndQuery.format(userId, query) }
+                                ?: templateQuery.format(query)
+                    } ?: userId?.let { templateUserId.format(userId) } ?: templateAll
             )
 
-    private fun runQuery(query: String): List<Item>? {
-        val session = entityManager.delegate as Session
-        val transaction = session.beginTransaction()
-        val items: List<Item>? = session.createQuery(query)
+    @Transactional(readOnly = true)
+    private fun runFilterQuery(query: String): List<Item>? {
+        return (entityManager.delegate as Session).createQuery(query)
                 ?.resultList
                 ?.map { it as Item }
-        transaction.commit()
-        return items
     }
 }
