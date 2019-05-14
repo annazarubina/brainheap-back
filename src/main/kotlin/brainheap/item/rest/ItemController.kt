1d62da16f002d51fc39429b043ca5bc7fe38e797
Anna Zarubina <anna.zarubina@ericsson.com>
2019-05-14 23:34:55 +0100

    #1 - refactoring + saveAll instaed of saving one by one

--- src/main/kotlin/brainheap/item/rest/ItemController.kt
+++ src/main/kotlin/brainheap/item/rest/ItemController.kt
@@ -10,7 +10,15 @@ import brainheap.user.repo.UserRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @Validated
@@ -32,16 +40,20 @@ class ItemController(private val repository: ItemRepository, private val service
     fun createAll(@RequestHeader(value = "Authorization") userId: String, @Valid @RequestBody itemViews: List<ItemView>):
             ResponseEntity<List<Item>> {
         require(userRepository.findById(userId).isPresent) { "User with this id ($userId) is not registered" }
-        return itemViews.map { repository.save(ItemProcessor.convert(it, userId)) }
+        return itemViews
+                .map { ItemProcessor.convert(it, userId) }
                 .takeIf { it.isNotEmpty() }
-                ?.let { ResponseEntity(it, HttpStatus.OK) } ?: ResponseEntity(HttpStatus.NO_CONTENT)
+                ?.let { repository.saveAll(it) }
+                ?.let { ResponseEntity(it, HttpStatus.OK) }
+                ?: ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
     @GetMapping("/items/{id}")
     fun get(@RequestHeader(value = "Authorization") userId: String, @PathVariable id: String): ResponseEntity<Item> {
         require(userRepository.findById(userId).isPresent) { "User with this id ($userId) is not registered" }
         return repository.findByUserIdAndId(userId, id)
-                ?.let { ResponseEntity(it, HttpStatus.OK) } ?: ResponseEntity(HttpStatus.NO_CONTENT)
+                ?.let { ResponseEntity(it, HttpStatus.OK) }
+                ?: ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
     @PostMapping("/items/new")
@@ -55,7 +67,9 @@ class ItemController(private val repository: ItemRepository, private val service
                @Valid @RequestBody itemView: ItemView): ResponseEntity<Item> {
         require(userRepository.findById(userId).isPresent) { "User with this id ($userId) is not registered" }
         return repository.findByUserIdAndId(userId, id)
-                ?.let { ResponseEntity(repository.save(ItemProcessor.update(it, itemView, userId)), HttpStatus.OK) }
+                ?.let { ItemProcessor.update(it, itemView, userId) }
+                ?.let { repository.save(it) }
+                ?.let { ResponseEntity(it, HttpStatus.OK) }
                 ?: ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
@@ -63,7 +77,8 @@ class ItemController(private val repository: ItemRepository, private val service
     fun delete(@RequestHeader(value = "Authorization") userId: String, @PathVariable id: String): ResponseEntity<Item> {
         require(userRepository.findById(userId).isPresent) { "User with this id ($userId) is not registered" }
         return repository.findByUserIdAndId(userId, id)
-                ?.let { ResponseEntity(deleteItem(it), HttpStatus.OK) } ?: ResponseEntity(HttpStatus.NO_CONTENT)
+                ?.let { ResponseEntity(deleteItem(it), HttpStatus.OK) }
+                ?: ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
     @DeleteMapping("/items")
@@ -72,7 +87,8 @@ class ItemController(private val repository: ItemRepository, private val service
         return repository.findByUserId(userId)
                 ?.takeIf { it.isNotEmpty() }
                 ?.map { deleteItem(it) }
-                ?.let { ResponseEntity(it, HttpStatus.OK) } ?: ResponseEntity(HttpStatus.NO_CONTENT)
+                ?.let { ResponseEntity(it, HttpStatus.OK) }
+                ?: ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
     private fun deleteItem(item: Item): Item {
