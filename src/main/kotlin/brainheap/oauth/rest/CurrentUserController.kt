34e3863a0a923cd26869a92cf749f66629e68583
Anna Zarubina <anna.zarubina@ericsson.com>
2019-07-10 09:35:20 +0100

    - add endpoint to redirect, add endpoint to get current user

---
+++ src/main/kotlin/brainheap/oauth/rest/CurrentUserController.kt
@@ -0,0 +1,30 @@
+package brainheap.oauth.rest
+
+import brainheap.oauth.security.CurrentUserDetector
+import brainheap.user.model.User
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
+
+@RestController
+class CurrentUserController(private val currentUserDetector: CurrentUserDetector) {
+    private val logger = LoggerFactory.getLogger(CurrentUserController::class.java)
+
+    @GetMapping("/redirect")
+    fun redirect(@RequestParam(name = "redirect_uri") redirectUri: String): RedirectView = RedirectView(redirectUri)
+
+    @GetMapping("/currentuser")
+    @ResponseBody
+    fun getCurrentuser(authentication: Authentication): ResponseEntity<User> {
+        return currentUserDetector.currentUser(authentication.principal as DefaultOAuth2User)
+                ?.let { ResponseEntity(it, HttpStatus.OK) }
+                ?: ResponseEntity(HttpStatus.NO_CONTENT)
+    }
+}
--- src/main/kotlin/brainheap/oauth/security/CurrentUserDetectorImpl.kt
+++ src/main/kotlin/brainheap/oauth/security/CurrentUserDetectorImpl.kt
@@ -1,5 +1,6 @@
 package brainheap.oauth.security
 
+import brainheap.user.model.User
 import brainheap.user.repo.UserRepository
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User
@@ -8,14 +9,25 @@ import org.springframework.stereotype.Component
 @Component
 class CurrentUserDetectorImpl(
         private val userRepository: UserRepository) : CurrentUserDetector {
+
     override fun currentUserId(authentication: OAuth2AuthenticationToken): String {
+        return currentUser(authentication.principal as DefaultOAuth2User)!!.id
+    }
+
+    override fun currentUserEmail(oAuth2User: DefaultOAuth2User?): String? {
         //todo add check for user not found
-        val user = authentication.principal as DefaultOAuth2User
-        val userEmail = user.attributes["email"].toString()
-        val currentUser = userRepository.findByEmail(userEmail)
+        require(oAuth2User != null) { "User is null" }
+        return oAuth2User.attributes["email"]?.run { toString() }
+    }
+
+    override fun currentUser(oAuth2User: DefaultOAuth2User?): User? {
+        val userEmail = currentUserEmail(oAuth2User)
+        require(userEmail != null ) { "User has no e-mail attribute" }
+        require(userEmail.isNotEmpty()) { "User e-mail is empty" }
+        val users = userRepository.findByEmail(userEmail)
         //todo throw UserNotFoundException
         //todo add exception handler to received http error code
-        require(currentUser.isNotEmpty()) { "User with this email ($userEmail) is not registered" }
-        return currentUser[0].id
+        require(users.isNotEmpty()) { "User with this email ($userEmail) is not registered" }
+        return users[0]
     }
 }
